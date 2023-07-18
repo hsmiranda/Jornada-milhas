f84dacfb7fb10198fd0798ae8f156fecdfe82c56
Herberson Miranda <herberson.miranda@outlook.com>
2023-07-18 20:26:34 -0300

    Criando projeto

---
+++ jornadamilhas/src/test/java/org/estudos/jornadamilhas/JornadaMilhasIT.java
@@ -0,0 +1,8 @@
+package org.estudos.jornadamilhas;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class JornadaMilhasIT extends JornadaMilhasTest {
+    // Execute the same tests but in packaged mode.
+}
