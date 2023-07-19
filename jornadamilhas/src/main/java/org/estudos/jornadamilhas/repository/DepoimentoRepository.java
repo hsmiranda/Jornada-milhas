21b7f99f741bfe25617ed191c490d5426757068a
Herberson Miranda <herberson.miranda@outlook.com>
2023-07-18 23:57:31 -0300

    CRUD api completo

--- jornadamilhas/src/main/java/org/estudos/jornadamilhas/repository/DepoimentoRepository.java
+++ jornadamilhas/src/main/java/org/estudos/jornadamilhas/repository/DepoimentoRepository.java
@@ -5,7 +5,11 @@ import java.util.List;
 import org.estudos.jornadamilhas.entity.Depoimento;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import io.quarkus.panache.common.Parameters;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.Response;
 
 @ApplicationScoped
 public class DepoimentoRepository implements PanacheRepository<Depoimento>{
@@ -19,7 +23,58 @@ public class DepoimentoRepository implements PanacheRepository<Depoimento>{
         return Depoimento.listAll();
     }
     
+    /**
+     * @param nome
+     * @return
+     */
+    public List<Depoimento> listByNome(String nome) {
+        return Depoimento.find("nomePessoa = :nome", Parameters.with("nomePessoa", nome)).list();
+    }
 
+    /**
+     * @param d
+     * @return
+     */
+    @Transactional
+    public Depoimento cadastrar(Depoimento d) {
+        Depoimento.persist(d);
+        return d;
+    }
+
+    /**
+     * @param id
+     * @param d
+     * @return
+     */
+    @Transactional
+    public Depoimento atualizar(Long id, Depoimento d) {
+
+        Depoimento depoimentoEntity = Depoimento.findById(id);
+
+        if(depoimentoEntity == null) {
+            throw new WebApplicationException("Depoimento nao encontrado", Response.Status.NOT_FOUND);
+        }
+
+        depoimentoEntity.setDepoimento(d.getDepoimento());
+        depoimentoEntity.setFoto(d.getFoto());
+        depoimentoEntity.setNomePessoa(d.getNomePessoa());
+
+        return depoimentoEntity;
+    }
+
+    /**
+     * @param id
+     */
+    @Transactional
+    public void apagar(Long id) {
+        Depoimento depoimentoEntity = Depoimento.findById(id);
+
+        if(depoimentoEntity == null) {
+            throw new WebApplicationException("Depoimento com o"+ id+ "nao encontrado", Response.Status.NOT_FOUND);
+        }
+
+        Depoimento.deleteById(depoimentoEntity.getId());
+    }
 
 
 }
--- jornadamilhas/src/main/java/org/estudos/jornadamilhas/resources/DepoimentoV2Resource.java
+++ jornadamilhas/src/main/java/org/estudos/jornadamilhas/resources/DepoimentoV2Resource.java
@@ -6,10 +6,12 @@ import org.estudos.jornadamilhas.entity.Depoimento;
 import org.estudos.jornadamilhas.repository.DepoimentoRepository;
 
 import jakarta.inject.Inject;
-import jakarta.transaction.Transactional;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -28,9 +30,23 @@ public class DepoimentoV2Resource {
     }
 
     @POST
-    @Transactional
     public Response create (Depoimento depoimento){
-        Depoimento.persist(depoimento);
+        depoimentoRepository.cadastrar(depoimento);
         return Response.ok(depoimento).status(Response.Status.CREATED).build();
+    }
+
+
+    @PUT
+    @Path("{id}")
+    public Response atualizar(@PathParam("id") Long id, Depoimento d) {
+        Depoimento depoimentoAtualizado = depoimentoRepository.atualizar(id, d);
+        return Response.ok(depoimentoAtualizado).build();
+    }
+
+    @DELETE
+    @Path("{id}")
+    public Response apagar(@PathParam("id") Long id){
+        depoimentoRepository.apagar(id);
+        return Response.status(Response.Status.NO_CONTENT).build();
     }
 }
