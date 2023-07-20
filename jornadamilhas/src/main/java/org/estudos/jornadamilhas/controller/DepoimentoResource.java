f1dd353a98174b4c6cee501dcba5f31deb62db77
Herberson Miranda <herberson.miranda@outlook.com>
2023-07-20 20:54:57 -0300

    Corrigindo Resource v1

--- jornadamilhas/src/main/java/org/estudos/jornadamilhas/controller/DepoimentoResource.java
+++ jornadamilhas/src/main/java/org/estudos/jornadamilhas/controller/DepoimentoResource.java
@@ -16,7 +16,7 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
-@Path("/depoimentos/v2")
+@Path("/depoimentos/v1")
 @Produces(MediaType.APPLICATION_JSON)
 public class DepoimentoResource {
 
